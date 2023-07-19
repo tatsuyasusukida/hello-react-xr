ed13a9d61014b79baee7eb3106f19417cb587a06
susukida <susukida@loremipsum.co.jp>
2023-07-19 10:36:47 +0900

    VR ボタンを追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,22 @@
 
 import { Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { VRButton, XR } from "@react-three/xr";
 
 export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="mt-4 mb-4 text-4xl">React Three XR</h1>
       <Canvas className="aspect-video border border-gray-300">
-        <Sphere>
-          <ambientLight intensity={0.1}></ambientLight>
-          <pointLight intensity={0.8} position={[10, 10, 10]}></pointLight>
-          <meshStandardMaterial color="#fafafa"></meshStandardMaterial>
-        </Sphere>
+        <XR>
+          <Sphere>
+            <ambientLight intensity={0.1}></ambientLight>
+            <pointLight intensity={0.8} position={[10, 10, 10]}></pointLight>
+            <meshStandardMaterial color="#fafafa"></meshStandardMaterial>
+          </Sphere>
+        </XR>
       </Canvas>
+      <VRButton></VRButton>
     </main>
   );
 }
