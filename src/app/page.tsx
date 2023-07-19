f56729e587af99e2ce604be344ed310d9f426f18
susukida <susukida@loremipsum.co.jp>
2023-07-19 11:18:10 +0900

    コントローラーと手を追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@
 
 import { Box, Cylinder, Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { VRButton, XR } from "@react-three/xr";
+import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 
 export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="mt-4 mb-4 text-4xl">React Three XR</h1>
+      <div className="mb-4">
+        <VRButton
+          style={{}}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        ></VRButton>
+      </div>
       <Canvas className="aspect-video border border-gray-300">
         <XR>
+          <Controllers />
+          <Hands />
           <ambientLight intensity={0.1}></ambientLight>
           <pointLight intensity={0.8} position={[10, 10, 10]}></pointLight>
           <Box args={[1, 1, 1]} position={[-1.5, 0, -5]}>
@@ -23,7 +31,6 @@ export default function Home() {
           </Cylinder>
         </XR>
       </Canvas>
-      <VRButton></VRButton>
     </main>
   );
 }
